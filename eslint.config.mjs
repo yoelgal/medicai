fd3f1676d6c4287f33949537c6106b644d7919cf
yoelgal <yoelgal108@gmail.com>
2025-06-01 19:30:22 +0100

    Fix ESLint configuration for flat config format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,8 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   ...compat.extends('prettier'),
+  ...compat.plugins('prettier'),
   {
-    plugins: ['prettier'],
     rules: {
       'prettier/prettier': 'error',
     },
